6821d600b3f6833a0049420822c745bd610feb0e
Edwing <macdo796@gmail.com>
2025-04-27 05:51:18 -0500

    llloooou

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post("/generar-cv", async (req, res) => {
   try {
     const data = req.body;
 
-    const templatePath = path.join(__dirname, "Hoja_de_Vida_Plantilla.docx");
+    const templatePath = path.join(__dirname, "Documento .docx");
 
     if (!fs.existsSync(templatePath)) {
       throw new Error("No se encontró la plantilla DOCX en el servidor.");
